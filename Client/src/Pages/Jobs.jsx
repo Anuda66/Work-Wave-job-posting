396c9c946eb77e97ab858b7047616459b8f34e76
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-02-24 22:31:57 +0530

    compleate al changes of css on all jobs page.

--- Client/src/Pages/Jobs.jsx
+++ Client/src/Pages/Jobs.jsx
@@ -25,14 +25,23 @@ function Jobs() {
 
   return (
     <div>
-      <p className='my-10 text-lg font-bold text-gray-600'>Broude through the job category.</p>
+      <div className='flex flex-col justify-between my-10 md:flex-row'>
+        <p className='text-lg font-bold text-gray-600 '>Broude through the job category.</p>
+        <button onClick={()=>navigate('/jobs')} className="px-8 py-3 mx-6 mt-6 font-light text-white transition duration-300 ease-in-out delay-150 bg-purple-300 rounded-full hover:-translate-y-1 hover:scale-110 hover:bg-purple-500 hover:shadow-lg md:mt-0">All Jobs</button>
+      </div>
       <div className='flex flex-col items-start gap-5 mt-5 sm:flex-row '>
         <div className='flex flex-col gap-4 text-gray-600 text-md'>
-          <p onClick={()=>JobTitle === 'Front-End Engineer' ? navigate('/jobs') : navigate('/jobs/Front-End Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer `}>Front-End Engineer</p>
-          <p onClick={()=>JobTitle === 'Back-End Engineer' ? navigate('/jobs') : navigate('/jobs/Back-End Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer `}>Back-End Engineer</p>
-          <p onClick={()=>JobTitle === 'Full Stack Engineer' ? navigate('/jobs') : navigate('/jobs/Full Stack Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer `}>Full Stack Engineer</p>
-          <p onClick={()=>JobTitle === 'Software Engineer' ? navigate('/jobs') : navigate('/jobs/Software Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer `}>Software Engineer</p>
-          <p onClick={()=>JobTitle === 'QA Engineer' ? navigate('/jobs') : navigate('/jobs/QA Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer `}>QA Engineer</p>
+          
+          <p onClick={()=>JobTitle === 'Front-End Engineer' ? navigate('/jobs') : navigate('/jobs/Front-End Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer ${JobTitle === "Front-End Engineer" ? "bg-purple-300 text-black" : ""}`}>Front-End Engineer</p>
+          
+          <p onClick={()=>JobTitle === 'Back-End Engineer' ? navigate('/jobs') : navigate('/jobs/Back-End Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer ${JobTitle === "Back-End Engineer" ? "bg-purple-300 text-black" : ""}`}>Back-End Engineer</p>
+          
+          <p onClick={()=>JobTitle === 'Full Stack Engineer' ? navigate('/jobs') : navigate('/jobs/Full Stack Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer ${JobTitle === "Full Stack Engineer" ? "bg-purple-300 text-black" : ""}`}>Full Stack Engineer</p>
+          
+          <p onClick={()=>JobTitle === 'Software Engineer' ? navigate('/jobs') : navigate('/jobs/Software Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer ${JobTitle === "Software Engineer" ? "bg-purple-300 text-black" : ""}`}>Software Engineer</p>
+          
+          <p onClick={()=>JobTitle === 'QA Engineer' ? navigate('/jobs') : navigate('/jobs/QA Engineer')} className={`w-[94Vw] sm:w-auto pl-3 py-1.5 pr-16 border border-e-gray-300 rounded transition-all cursor-pointer ${JobTitle === "QA Engineer" ? "bg-purple-300 text-black" : ""}`}>QA Engineer</p>
+          
         </div>
         <div className='grid w-full gap-4 grid-cols-auto gap-y-6'>
           {
