4a99b8e6ddc65cef95fbadee60c7bf53192349f3
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-02-27 18:55:37 +0530

    create admin controler.

---
+++ Backend/controllers/adminController.js
@@ -0,0 +1,15 @@
+
+
+
+// Api for adding Jobs from adming -----------------------
+
+const addJob = async (resizeBy, req) => {
+    try{
+        const {JobTitle,companyName,dis,requiremat,techlnolegy,date} = req.body
+    }
+    catch(error){
+
+    }
+}
+
+export {addJob}
