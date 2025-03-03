2443bfb81d5ad8c9e626178d0e060341cedb4e2b
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-04 00:35:43 +0530

    Compleate auth Admin contriller.

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { addJob, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
+import authAdmin from '../middlewares/authAdmin.js'
+
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-job', upload.single('image'), addJob)
+adminRouter.post('/add-job',authAdmin ,upload.single('image'), addJob)
 adminRouter.post('/login', loginAdmin)
 
 export default adminRouter
