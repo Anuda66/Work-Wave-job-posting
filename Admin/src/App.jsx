827830cc22f7bb069b8ca7bf71ca1d5efdbb3131
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-05 00:21:36 +0530

    create ap context file

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-
+import Login from "./Pages/Login"
 
 function App() {
   
-
   return (
-    <>
-      
-    </>
+    <div>
+      <Login/>
+    </div>
   )
 }
 
---
+++ Admin/src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      jhgjhdf
+    </div>
+  )
+}
+
+export default Login
