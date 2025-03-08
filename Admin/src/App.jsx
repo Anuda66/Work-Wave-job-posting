7d757a007f889566f4287d514ac92a8b9f2d58e2
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-08 23:53:18 +0530

    create login and add Toastify.

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,10 +1,12 @@
 import Login from "./Pages/Login"
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   
   return (
     <div>
       <Login/>
+      <ToastContainer/>
     </div>
   )
 }
--- Admin/src/Pages/Login.jsx
+++ Admin/src/Pages/Login.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react'
+import { AdminContext } from '../Context/AdminContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 function Login() {
 
   const [state, setState] = useState('Admin')
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const {setAToken, backendUrl} = useContext(AdminContext)
+
+  const onSubmitHandler = async(event) => {
+
+    event.preventDefault() // when submit form not reloade page.
+
+    try{
+      if (state === 'Admin'){
+        const {data} = await axios.post(backendUrl + '/api/admin/login',{email, password})
+        if(data.success){
+          localStorage.setItem('aToken', data.token)
+          setAToken(data.token);
+        }
+        else{
+          toast.error(data.message)
+        }
+      }
+      else{
+        
+      }
+        
+    }
+    catch (error){
+      
+    }
+  }
 
   return (
-    <form className='min-h-[80vh] flex items-center' >
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center' >
       <div className='flex flex-col items-start gap-3 p-8 m-auto min-w-[340px] sm:min-w-96 border rounded-2xl text-gray-700 tex-sm shadow-lg border-gray-200'>
         <p className='m-auto text-2xl font-semibold '><span className='text-purple-500'>{state}</span> Login</p>
 
         <div className='w-full'>
           <p className='text-sm text-gray-600'>Email</p>
-          <input className='w-full p-2 mt-1 text-sm text-gray-600 border border-gray-300 rounded-md' type="email" required/>
+          <input onChange={(e)=>setEmail(e.target.value)} value={email} className='w-full p-2 mt-1 text-sm text-gray-600 border border-gray-300 rounded-md' type="email" required/>
         </div>
 
         <div className='w-full'>
           <p className='text-sm text-gray-600'>Password</p>
-          <input className='w-full p-2 mt-1 text-sm text-gray-600 border border-gray-300 rounded-md' type="password" required/>
+          <input onChange={(e)=>setPassword(e.target.value)} value={password} className='w-full p-2 mt-1 text-sm text-gray-600 border border-gray-300 rounded-md' type="password" required/>
         </div>
 
         <button className='w-full pt-2 pb-2 text-base text-white transition duration-300 ease-in-out delay-150 bg-purple-500 rounded-2xl bg-primary hover:bg-purple-800 hover:shadow-lg hover:-translate-y-1 hover:scale-110'>Login</button>
