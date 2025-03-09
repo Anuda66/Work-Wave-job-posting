288b0cc277a25681aab3890722e3fbd84a763f52
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-09 19:31:33 +0530

    Add new pages.

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -3,6 +3,11 @@ import Login from "./Pages/Login"
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from "./Context/AdminContext";
 import Navbar from "./Components/Navbar";
+import SideBar from "./Components/SideBar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Pages/Admin/Dashboard";
+import AllJobs from "./Pages/Admin/AllJobs";
+import AddJobs from "./Pages/Admin/AddJobs";
 
 function App() {
   
@@ -12,6 +17,15 @@ function App() {
     <div className="bg-[#f8f9fd]">
       <ToastContainer/>
       <Navbar/>
+      <div className="flex items-start">
+        <SideBar/>
+        <Routes>
+          <Route path="/" element={<></>}/>
+          <Route path="/admin-dashborad" element={<Dashboard/>}/>
+          <Route path="/all-jobs" element={<AllJobs/>}/>
+          <Route path="/add-jobs" element={<AddJobs/>}/>
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
---
+++ Admin/src/Pages/Admin/AddJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddJobs() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddJobs
