96831abd4944eb8169735d2ad81b3c3c948df128
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-09 15:03:54 +0530

    create logout function.

--- Admin/src/Components/Navbar.jsx
+++ Admin/src/Components/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AdminContext } from "../Context/AdminContext";
+import logo from '../assets/logo.png'
+import {useNavigate} from 'react-router-dom'
 
 function Navbar() {
+  
+  const { aToken, setAToken } = useContext(AdminContext);
+
+  const navigate = useNavigate()
+  
+  const logout = () => {
+    navigate('/')
+    aToken && setAToken('')
+    aToken && localStorage.removeItem('aToken')
+  }
+
   return (
-    <div>
-      <div>
-        <img src='' alt='logo'/>
-        <p>{aToken ? 'Admin' : 'Company'}</p>
+    <div className="flex items-center justify-between px-4 py-3 bg-purple-600 shadow-xl sm:px-10">
+      <div className="flex items-center gap-8 text-xs">
+        <img className="w-24 cursor-pointer" src={logo} alt="logo" />
+        <p className="border px-2.5 py-0.5 rounded-full border-white text-white">{aToken ? "Admin" : "Company"}</p>
       </div>
-      <button>Logout</button>
+      <button onClick={logout} className="px-10 py-2 font-light text-purple-600 transition duration-300 ease-in-out delay-150 bg-white rounded-full cursor-pointer md:block hover:-translate-y-1 hover:scale-110 hover:shadow-lg">Logout</button>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
