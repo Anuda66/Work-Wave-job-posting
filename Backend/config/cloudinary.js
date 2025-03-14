34b6c288d321ed10ebdbbded7f352511b5f0f24b
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-15 01:36:45 +0530

    Create coludnary error.

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -5,7 +5,7 @@ dotenv.config(); // Load environment variables from .env file
 
 const connectCloudinary = async () => {
   cloudinary.config({
-    cloude_name: process.env.CLOUDINARY_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECROTE_KEY,
   });
--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,38 +1,49 @@
 import {v2 as cloudinary} from 'cloudinary'
 import jobModel from '../models/jobModel.js'
 import jwt from 'jsonwebtoken'
+import validator from'validator'
+
 
 // Api for adding Jobs from admin -----------------------
 
 const addJob = async (req, res) => {
     try {
-      const { JobTitle, comName, discription, requiremat, techlnolegy, date , link} = req.body;
-      // const imageFile = req.file;
+      const {comName,  jobTitel, technology, discription, requirements, email, link} = req.body;
+      const imageFile = req.file;
   
       // Check if all required fields are present
-      if (!JobTitle || !comName || !discription || !requiremat || !techlnolegy || !link) {
+
+      if ( !comName || !jobTitel  || !technology || !discription || !requirements || !email || !link) {
         return res.json({ success: false, message: "Missing data" });
+      }
+
+      //console.log({comName,  jobTitel, technology, discription, requirements, email, link},imageFile);
+      
+      if (!validator.isEmail(email)){
+        return res.json({ success: false, message: "Invalid emai" });
       }
   
       // Check if a file was uploaded
-     // if (!imageFile) {
-      //  return res.json({ success: false, message: "No file uploaded" });
-     // }
+      if (!imageFile) {
+        return res.json({ success: false, message: "No file uploaded" });
+     }
   
       // Upload image to Cloudinary
-      // const imageUploade = await cloudinary.uploader.upload(imageFile.path, {resource_type: 'image'});
-      // const imageUrl = imageUploade.secure_url;
+      const imageUploade = await cloudinary.uploader.upload(imageFile.path, {resource_type: 'image'});
+      const imageUrl = imageUploade.secure_url;
   
       // Add data to the database
       const jobData = {
-        JobTitle,
-        comName, 
+        
+        comName,
+        jobTitel, 
+        technology,
         discription,
-        requiremat,
-        techlnolegy,
+        requirements,
+        email,
         link,
         date: Date.now(),
-        //image: imageUrl
+        image: imageUrl
       };
   
       const newJob = new jobModel(jobData);
