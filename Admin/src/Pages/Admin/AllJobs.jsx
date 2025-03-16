4914df196b4d096905941bb844aeded1d21d25f5
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-17 01:08:36 +0530

    Add Google font and change css on all jobs card

--- Admin/src/Pages/Admin/AllJobs.jsx
+++ Admin/src/Pages/Admin/AllJobs.jsx
@@ -12,13 +12,13 @@ function AllJobs() {
   },[aToken])
 
   return (
-    <div className='m-5 max-h-[80vh] overflow-scroll'>
-      <p className='mb-10 text-xl'>All Jobs</p>
-      <div className='flex flex-wrap w-full gap-4 pt-5 gap-y-6'>
+    <div className='m-5 max-h-[90vh] overflow-scroll'>
+      <p className='text-xl '>All Jobs</p>
+      <div className='flex flex-wrap w-full gap-4 pt-5 gap-y-6 '>
         {
          jobs && Array.isArray(jobs) && jobs.map((item, index) =>{
           return(
-            <div className='flex flex-wrap w-full gap-4 p-5 pt-3 mt-10 border-gray-400 shadow-xl cursor-pointer border-1 rounded-2xl gap-y-6' key={index}>
+            <div className='p-3 overflow-hidden border-gray-400 cursor-pointer border-1 rounded-xl max-w-96 group' key={index}>
               <div className='space-y-1.5'>
                <div className='flex items-center gap-5'>
                 <img className='w-10 h-10 rounded-full' src={item.image} alt=''/>
@@ -30,10 +30,10 @@ function AllJobs() {
                   <p className='text-base text-purple-700'>{item.technology}</p>
                 </div>
                   <p className='text-md '>About the Role:</p>
-                  <p className='text-gray-500 text-md'>{item.discription}</p>
+                  <p className='text-sm text-gray-500'>{item.discription}</p>
 
                   <p>Requirements:</p>
-                  <p className='text-gray-500 text-md'>{item.requirements}</p>
+                  <p className='text-sm text-gray-500'>{item.requirements}</p>
               </div>
             </div>
           )
