16adf0ffe38b32117e4fdb3547fdf545edfd2eda
anudaransara <148308653+Anuda66@users.noreply.github.com>
2025-03-21 23:59:08 +0530

    create get profile route path.

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { registerUser, loginUser } from '../controllers/userController.js'
+import { registerUser, loginUser, getProfile } from '../controllers/userController.js'
+import authUser from '../middlewares/authUser.js'
 
 const UserRouter = express.Router()
 
 UserRouter.post('/register', registerUser)
 UserRouter.post('/login', loginUser)
 
-
+UserRouter.get('/get-profile', authUser, getProfile)
 
 
 
