d7b6507fb0012445881f299ec983f3432173ef0c
Anuda Ransara <anudaransara1@gmail.com>
2025-04-04 23:32:37 +0530

    Add function on Cv fromat oage.

--- Client/src/Pages/CVFormat.jsx
+++ Client/src/Pages/CVFormat.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CVFormat() {
+
+  const [userData, setUserData] = useState({
+    name : 'Amuda Ransara',
+    image : '',
+    email : 'anudaransara@gmail.com',
+    
+  })
   return (
     <div>
-      fvdsfdsf
+      
     </div>
   )
 }
