ceb37eac784aed908c33c3e77cbd09f214f0fe52
Anuda Ransara <anudaransara1@gmail.com>
2025-04-14 20:49:03 +0530

    Create candidate profile page.

--- Client/src/Pages/CandidateProfile.jsx
+++ Client/src/Pages/CandidateProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {AppContext} from '../Context/AppContext'
 import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
@@ -10,11 +10,24 @@ import { useState } from 'react'
 function CandidateProfile() {
 
   const { userId } = useParams()
-   const {users} = useContext(AppContext)
+  const {users} = useContext(AppContext)
+
+  const [userInfo, setUserInfo] = useState(null)
+
+  const fetchUserInfo = async() => {
+    const userInfo = users.find((user) => user._id === userId)
+    setUserInfo(userInfo)
+    console.log(userInfo);
+    
+  }
+
+  useEffect(()=>{
+    fetchUserInfo()
+  },[users,userId])
   
   return (
     <div>
-      vvsdv
+      
     </div>
   )
 }
